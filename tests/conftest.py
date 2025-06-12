a3ee1b9e8572069f2c6454cb9ba543ddde2188ff
tejas <tejaschendekar@yahoo.com>
2025-06-12 16:33:39 +0100

    Fixed sessionlocal issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
+from starlette.testclient import TestClient
+
+from app.api.core import get_db
 from app.db.base_class import Base
 import os
+
+from app.main import app
 
 TEST_DATABASE_URL = os.getenv(
     'TEST_DATABASE_URL', 'postgresql://postgres:waffle@localhost:5432/eurus_test'
@@ -34,3 +39,20 @@ def db_session(engine, tables):
     session.close()
     transaction.rollback()
     connection.close()
+
+
+@pytest.fixture
+def client(db_session):
+    def get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = get_test_db
+
+    with TestClient(app) as test_client:
+        yield test_client
+
+    # Clean up the override after testing
+    app.dependency_overrides.clear()
